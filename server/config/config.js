32a7210f2c0503d553d616cef9bde5aa64a91cf1
DahirMolina <dahir.slash@gmail.com>
2018-10-08 14:46:58 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,12 +15,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ========================
 
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
 
-///  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb://cafe-user:A123456@ds053196.mlab.com:53196/cafe';
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
 
 
-//}
+}
 process.env.URLDB = urlDB;
